424dcf0a17df4957008002f2c578efb3df8789bb
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-11-27 23:09:22 -0500

    updated the BookSearch component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "../src/styles/setup.css";
 import "../src/styles/App.scss";
 
-import React, { Component } from "react";
+import React from "react";
 import BookSearch from "./components/BookSearch/BookSearch";
 
-class App extends Component {
-  render() {
-    return <BookSearch />;
-  }
+function App() {
+  return (
+    <div>
+      <BookSearch />;
+    </div>
+  );
 }
 
 export default App;
-
--- src/components/BookSearch/BookSearch.js
+++ src/components/BookSearch/BookSearch.js
@@ -3,17 +3,6 @@ import React from "react";
 import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
 
-// class BookSearch extends Component {
-//   render() {
-//     return (
-//       <div className="container">
-//         <Header />
-//         <SearchForm />
-//       </div>
-//     );
-//   }
-// }
-
 function BookSearch() {
   return (
     <div className="container">
